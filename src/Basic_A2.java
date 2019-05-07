d66a5a42a2518659f8cf3ec05912085ff2cc8beb
Simon Blaha <s.blaha@live.at>
2019-05-07 14:23:24 +0200

    merge commit

--- src/Basic_A2.java
+++ src/Basic_A2.java
@@ -1,30 +1,30 @@
-import java.util.Scanner;
+    import java.util.Scanner;
 
-public class Basic_A2   {
+    public class Basic_A2   {
 
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        float n;
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter any number ");
-        n = s.nextFloat();
+            float n;
+            Scanner s = new Scanner(System.in);
+            System.out.print("Enter any number ");
+            n = s.nextFloat();
 
-        if(n == 0) {
-            System.out.println("The number " + n + " is zero");
-        } else if (n < 1) {
-            if(n < 0) {
-                System.out.println("The number " + n + "is negative");
+            if(n == 0) {
+                System.out.println("The number " + n + " is zero");
+            } else if (n < 1) {
+                if(n < 0) {
+                    System.out.println("The number " + n + "is negative");
+                } else {
+                    System.out.println("The number " + n + " is positive small");
+                }
+            } else if (n > 1000000) {
+                System.out.println("The number " + n + " is large positive");
             } else {
-                System.out.println("The number " + n + " is positive small");
+                System.out.println("The number " + n + " is positive");
             }
-        } else if (n > 1000000) {
-            System.out.println("The number " + n + " is large positive");
-        } else {
-            System.out.println("The number " + n + " is positive");
+
         }
 
     }
-
-}
 
